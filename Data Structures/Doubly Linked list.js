6f338694776386ddfd601dc9ddcd05a31d754410
Aman <iemamyaman007@gmail.com>
2022-05-06 11:46:26 +0530

    dll-skeleton

---
+++ Data Structures/Doubly Linked list.js
@@ -0,0 +1,15 @@
+class Node{
+    constructor(val){
+        this.value= val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = null;
+    }
+}
