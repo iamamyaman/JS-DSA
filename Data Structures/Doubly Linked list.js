5ed2000f13ef2d308d02a88b05c9143b50b32831
Aman <iemamyaman007@gmail.com>
2022-05-07 13:20:11 +0530

    DLL done

--- Data Structures/Doubly Linked list.js
+++ Data Structures/Doubly Linked list.js
@@ -93,6 +93,15 @@ class DoublyLinkedList{
         }
     }
 
+    set(index,val){
+        var foundNode = this.get(index);
+        if(foundNode!=null){
+            foundNode.val = val;
+            return true;
+        }
+        return false;
+    }
+
 }
 
 var list = new DoublyLinkedList();
